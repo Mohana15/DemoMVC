ab3e36d55440dfb6f5e3f80eeb94b95eb9a162c7
ranjith1525 <ranjith.kantipudi@gmail.com>
2017-11-29 12:26:18 -0500

    DEMO MVC

---
+++ DemoMVC/src/main/resources/com/fss/AddController.java
@@ -0,0 +1,13 @@
+package com.fss;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AddController {
+
+	@RequestMapping("/add")
+	public void add() {
+		System.out.println("hey this is add controller");
+	}
+}
